a506500ab8d5066e06e6094e74eaf148db02fa48
Ida Iyes <identry@protonmail.com>
2023-07-10 18:02:00 +0300

    add dummy example for testing

---
+++ examples/dummy.rs
@@ -0,0 +1,35 @@
+#![allow(dead_code)]
+
+use bevy::prelude::*;
+use iyes_progress::{prelude::*, dummy_system_wait_frames, dummy_system_wait_millis};
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default, States)]
+enum AppState {
+    #[default]
+    Loading,
+    MainMenu,
+}
+
+fn main() {
+    App::new()
+        // Init bevy
+        .add_plugins(DefaultPlugins)
+        // Add our state type
+        .add_state::<AppState>()
+        .add_plugins(
+            ProgressPlugin::new(AppState::Loading)
+                .continue_to(AppState::MainMenu)
+        )
+        // Our game loading screen
+        // systems that implement tasks to be tracked for completion:
+        .add_systems(
+            Update,
+            (
+                dummy_system_wait_frames::<50>.track_progress(),
+                dummy_system_wait_millis::<500>.track_progress(),
+            )
+                .run_if(in_state(AppState::Loading)),
+        )
+        .run();
+}
+
